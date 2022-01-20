699be86141a1dd91fe481f1f83a793c9e01806ee
unlocomqx <amxmodx.unloco@gmail.com>
2022-01-21 00:02:00 +0100

    First commit

---
+++ cypress/integration/ffmpeg.spec.js
@@ -0,0 +1,8 @@
+describe('ffmpeg test', ()=>{
+
+  it("should load page without errors", () => {
+    cy
+    .visit('http://localhost:3000')
+  })
+
+})
---
+++ main.js
@@ -0,0 +1,5 @@
+import { createFFmpeg } from "@ffmpeg/ffmpeg"
+
+const ffmpeg = createFFmpeg({ log: true })
+
+ffmpeg.load()
